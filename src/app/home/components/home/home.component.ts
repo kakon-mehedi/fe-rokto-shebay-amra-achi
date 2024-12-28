1bd82fbdef22a6edf3dcd83662ca7278c838018b
kakon-selise <kakon.mehedi@selisegroup.com>
2024-12-28 11:13:31 +0600

    feat: Add updates

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -2,35 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { IModule } from '../../interfaces/module-properties.interface';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+	modules: IModule[] = [
+		{
+			title: 'নতুন ডোনার ফর্ম ফিল আপ',
+			url: '/users/register',
+			icon: 'register',
+		},
 
-  modules:IModule[] = [
-    {
-      title: 'নতুন ডোনার ফর্ম ফিল আপ',
-      url: '/users/register',
-      icon: 'register'
-    },
+		// {
+		// 	title: 'এডমিন লগইন',
+		// 	url: '/users/login',
+		// 	icon: 'register',
+		// },
 
-    {
-      title: 'এডমিন লগইন',
-      url: '/users/login',
-      icon: 'register'
-    },
-
-    {
-      title: 'ইমারজেন্সি যোগাযোগ',
-      url: '/dashboard',
-      icon: 'register'
-    }
-  ]
-
+		{
+			title: 'ডোনার লিস্ট ',
+			url: '/dashboard',
+			icon: 'register',
+		},
+	];
 }
